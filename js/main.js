eec5812e7055a37b9d8618eb064aa7d0f9444273
Pamela Sorrells <pamelan.sorrells@gmail.com>
2022-03-18 21:30:47 -0500

    added roll calculator

--- js/main.js
+++ js/main.js
@@ -6,3 +6,24 @@ function makePerson(i,s,c,d) {
     return {intelligence: i, strength: s, constitution: c, dexterity: d, location: 'floor'}
 }
 
+
+function calculateRoll(ability) {
+    let natRoll = Math.ceil(Math.random * 20)
+    let critStatus;
+    switch (roll) {
+        case 20:
+            critStatus = "success";
+            break;
+        case 1:
+            critStatus = "failure";
+            break;
+        default:
+            critStatus = "none";
+            break;
+    }
+    let thisCheck = {
+        roll: natRoll + ability,
+        crit: critStatus
+    }
+    return thisCheck
+}
