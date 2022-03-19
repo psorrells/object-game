fa18fdd772bc32a2edec78fa30e683eb30bc26b1
Pamela Sorrells <pamelan.sorrells@gmail.com>
2022-03-18 22:14:04 -0500

    turned actions into object

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-const actions = ["study","move","eat","jump on"]
+const actions = {
+    "study": "intelligence",
+    "move": "strength",
+    "eat": "constitution",
+    "jump on": "dexterity",
+}
 
 let person;
 
@@ -6,6 +11,8 @@ let person;
 //Chair
 const chair = {
     "study": {
+        "successRoll": 16,
+        "moderateRoll": 9,
         "critical failure": "You have no idea how this works. As you attempt to study the chair, you pull a screw loose and the whole chair collapses.",
         "failure": "You can't figure out anything about the chair. You sit on it and ponder why sit on a chair, when you could just sit on the floor? Are chairs useful?",
         "moderate": "It's a solid wooden chair. Maybe if you did something else with it, you could get out of here.",
@@ -13,6 +20,8 @@ const chair = {
         "critical success": "You look closely at the chair, as you assess all the angles and configurations, you realize there's a pattern on the legs. You turn each leg one by one, and look! The chair starts hovering. you can fly your way out of here!"
     },
     "move": {
+        "successRoll": 16,
+        "moderateRoll": 9,
         "critical failure": "You try to pick up the chair and break your hip. Should've stretched first.",
         "failure": "You try to pick up the chair, but it's much heavier than you assumed. You can only move it a few inches.",
         "moderate": "You move the chair. It's pretty solid, and surprisingly heavy, but you manage to move it to the window.",
@@ -20,6 +29,8 @@ const chair = {
         "critical success": "You throw the chair as hard as you can. It breaks the wall, and you are free."
     },
     "eat": {
+        "successRoll": 16,
+        "moderateRoll": 9,
         "critical failure": "You try to take a huge bite out of that deliciously looking solid oak chair. You crack your teeth.",
         "failure": "Somehow, you manage to bite off a few splinters of wood, but those splinters don't feel so good on the way down.",
         "moderate": "You try to eat the chair, but it is solid oak. You can't eat it.",
@@ -27,12 +38,19 @@ const chair = {
         "critical success": "You eat the chair. Your stomach knows no bounds. The chair is gone, but you are now made of wood and able punch your way through the wall out of here."
     },
     "jump on": {
+        "successRoll": 16,
+        "moderateRoll": 9,
         "critical failure": "You try to jump on the chair, but as your foot hits the edge, it shatters. You fall on the floor and cry for hours.",
         "failure": "You try to jump on the chair, but are unable to get a good height and miss.",
         "moderate": "You try to jump on the chair. It wobbles for a second as your first foot lands, but stabilizes.",
         "success": "You jump on the chair with finesse. This was an easy task for you.",
         "critical success": "You jump on the chair with a jump so powerful that it shatters the earth beneath you and frees you from the walls you are confined in."
     },
+    interact(person, action) {
+        let s = this[action]["successRoll"]
+        let m = this[action]["moderateRoll"]
+        let check = doAbilityCheck
+    }
 }
 
 function makePerson(i,s,c,d) {
