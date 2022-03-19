346a9534a81ba67d09b77062caf28688271f7114
Pamela Sorrells <pamelan.sorrells@gmail.com>
2022-03-18 21:20:56 -0500

    added actions array and make person function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+const actions = ["study","move","eat","jump on"]
+
+let person;
+
+function makePerson(i,s,c,d) {
+    return {intelligence: i, strength: s, constitution: c, dexterity: d, location: 'floor'}
+}
 
